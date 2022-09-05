c6b1ce7d97178b1a6d702010e6e17963f909728b
Esipov Aleksey <a_esipov@list.ru>
2022-09-05 16:32:10 +0300

    2. HQL [#6874 #306572]

---
+++ src/main/java/ru/job4j/cars/repository/UserUsage.java
@@ -0,0 +1,42 @@
+package ru.job4j.cars.repository;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.cars.model.User;
+
+public class UserUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (SessionFactory sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            UserRepository userRepository = new UserRepository(sf);
+            User userOne = new User();
+            userOne.setLogin("admin");
+            userOne.setPassword("admin");
+            userRepository.create(userOne);
+            User userTwo = new User();
+            userTwo.setLogin("moder");
+            userTwo.setPassword("moder");
+            userRepository.create(userTwo);
+            userRepository.findAllOrderById()
+                    .forEach(System.out::println);
+            userRepository.findByLikeLogin("e")
+                    .forEach(System.out::println);
+            userRepository.findById(userOne.getId())
+                    .ifPresent(System.out::println);
+            userRepository.findByLogin("admin")
+                    .ifPresent(System.out::println);
+            userOne.setPassword("password");
+            userRepository.update(userOne);
+            userRepository.findById(userOne.getId())
+                    .ifPresent(System.out::println);
+            userRepository.delete(userOne.getId());
+            userRepository.findAllOrderById()
+                    .forEach(System.out::println);
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
